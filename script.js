ef582128613c77b349aa7961e9bb7c0e71a391df
Chris <chunt661@mtroyal.ca>
2023-03-17 20:45:00 -0600

    Added jackpot parsing and display function

--- script.js
+++ script.js
@@ -11,3 +11,26 @@ const LOTTO_IDS = [
 document.addEventListener("DOMContentLoaded", function() {
     
 });
+
+function updateJackpot(id, jackpot) {
+    let number = jackpot; // Number to be displayed
+    let unit = ""; // The million/billion display
+
+    if (jackpot >= 10e8) {
+        unit = "BILLION";
+        number = jackpot / 10e8;
+        // Truncates decimals beyond the tenths place
+        number = Math.trunc(number*10)/10;
+    } else if (jackpot >= 10e5) {
+        unit = "MILLION";
+        number = Math.floor(jackpot / 10e5);
+    } else {
+        // Adds commas to the number
+        number = Math.floor(number).toLocaleString("en-US");
+    }
+    
+    document.querySelectorAll(`.lotto-${id}`).forEach(lotto => {
+        lotto.querySelector(".jackpot").textContent = number;
+        lotto.querySelector(".illion").textContent = unit;
+    });
+}
