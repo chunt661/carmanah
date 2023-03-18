19369561a77c1285e53a0f43aa924b76f723e1a0
Chris <chunt661@mtroyal.ca>
2023-03-17 21:21:56 -0600

    Commenting

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const FEED_API = "https://challenge.carmanahsigns.com/";
-const FEED_INTERVAL = 60; // Seconds
+// How often the feed will be refreshed, in seconds
+const FEED_INTERVAL = 60;
 
+/*
+List of lottery identifiers. Used for iterating through the different jackpots
+and ensuring that each is updated every cycle. This must be maintained on the
+client side, as the API does not consistently return all lotteries and
+therefore some jackpots may be missed during an update.
+*/
 const LOTTO_IDS = [
     "lmax",
     "l649",
@@ -9,6 +16,7 @@ const LOTTO_IDS = [
 ];
 
 document.addEventListener("DOMContentLoaded", function() {
+    // Generate markup at each location marked in the HTML file
     LOTTO_IDS.forEach(id => {
         document.querySelectorAll(`.lotto-${id}`).forEach(tag => {
             tag.appendChild(createJackpotMarkup(id));
@@ -16,7 +24,10 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
-function createJackpotMarkup(id) {
+/**
+Creates and returns a DOM element to be used for displaying jackpots.
+*/
+function createJackpotMarkup() {
     const container = document.createElement("div");
     
     const jackpotContainer = document.createElement("div");
@@ -39,6 +50,13 @@ function createJackpotMarkup(id) {
     return container;
 }
 
+/**
+Updates DOM elements to display the given jackpot. The jackpot is formatted and
+the accompanying unit (i.e., million or billion) is displayed when necessary.
+
+@param id The identifer for the lottery being updated.
+@param jackpot The jackpot dollar amount.
+*/
 function displayJackpot(id, jackpot) {
     let number = jackpot; // Number to be displayed
     let unit = ""; // The million/billion display
