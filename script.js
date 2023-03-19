8a9c954467dbb039d4a3cfbe6b94d5cb899f5137
Chris <chunt661@mtroyal.ca>
2023-03-18 18:20:10 -0600

    Takeover transition animation

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const FEED_API = "https://challenge.carmanahsigns.com/";
 // How often the feed will be refreshed, in seconds
 const FEED_INTERVAL = 60;
 // How often the takeover video will be displayed, in seconds
-const TAKEOVER_INTERVAL = 10;
+const TAKEOVER_INTERVAL = 30;
 
 /*
 List of lottery identifiers. Used for iterating through the different jackpots
@@ -169,32 +169,72 @@ function showTakeoverVideo() {
     const video = container.querySelector("video");
     
     container.classList.remove("hidden");
+    
+    createTakeoverAnimation(1, 8, false, () => {
+        // Remove animation from feed tiles after transition finishes
+        document.querySelectorAll(".tile").forEach(tile => {
+            tile.classList.add("hidden");
+            tile.classList.remove("tile-animation");
+        });
+    });
+    
+    video.classList.add("takeover-video-animation");
     video.play();
     
     // Stop the side videos
     document.querySelectorAll(".side-video video").forEach(v => v.pause());
-    
-    // Remove animation from feed tiles
-    document.querySelectorAll(".tile").forEach(tile => {
-        tile.classList.remove("tile-animation");
-    });
 }
 
 function hideTakeoverVideo() {
     const container = document.querySelector("#takeover-video");
+    
     container.classList.add("hidden");
     
-    // Resume the side videos from the beginning
-    document.querySelectorAll(".side-video video").forEach(v => {
-        v.currentTime = 0;
-        v.play();
+    createTakeoverAnimation(.3, 1, true, () => {
+        // Show the feed tile animation after the transition finishes
+        document.querySelectorAll(".tile").forEach(tile => {
+            tile.classList.remove("hidden");
+            tile.classList.add("tile-animation");
+        });
     });
     
-    // Show the feed tile animation
-    document.querySelectorAll(".tile").forEach(tile => {
-        tile.classList.add("tile-animation");
-    });
+     // Resume the side videos from the beginning
+    document.querySelectorAll(".side-video video").forEach(v => v.play());
     
     // Restart the takeover timer
     startTakeoverTimer();
+}
+
+/**
+Populates the transition overlay container with elements that play an
+animation.
+
+@param duration The duration of the animation, in seconds
+@param numCircles How many circle animations to generate
+@param reversed Whether the animation should play in reverse
+@param callback The function to be executed when the animation completes
+*/
+function createTakeoverAnimation(duration, numCircles, reversed, callback) {
+    const overlay = document.querySelector("#takeover-overlay");
+    
+    // Create circles for transition animation
+    for (let i = 0; i < numCircles; i++) {
+        let circle = document.createElement("div");
+        circle.classList.add("circle-transition");
+        circle.style.animationDuration = duration + "s";
+        circle.style.animationDelay = (.05 * i) + "s";
+        circle.style.animationDirection = reversed ? "reverse" : "normal";
+
+        overlay.appendChild(circle);
+        
+        // Clear animation elements when final animation ends
+        if (i == numCircles-1) {
+            circle.addEventListener("animationend", () => {
+                overlay.innerHTML = "";
+                if (callback) {
+                    callback();
+                }
+            });
+        }
+    }
 }
